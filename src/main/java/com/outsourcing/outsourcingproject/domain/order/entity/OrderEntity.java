5ad6e84df99e44ef515627eafb811ede767366b0
Jess <jcscene@gmail.com>
2025-04-23 22:02:39 +0900

    feat : OrderEntity, OrderRepository 구현

--- src/main/java/com/outsourcing/outsourcingproject/domain/order/entity/OrderEntity.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/order/entity/OrderEntity.java
@@ -1,4 +1,29 @@
 package com.outsourcing.outsourcingproject.domain.order.entity;
 
-public class OrderEntity {
+import com.outsourcing.outsourcingproject.common.entity.BaseEntity;
+import com.outsourcing.outsourcingproject.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "order")
+public class OrderEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY) // order.getUser(); 할 때 쿼리 발생시키기 위함
+	@JoinColumn(nullable = false)
+	private User user;
+
 }
