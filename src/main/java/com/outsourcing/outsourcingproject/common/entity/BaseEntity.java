08022f9955ad34e6eb8b04e9ce3fb71bbc216d86
yunhyeok_lee <yuy9611@gmail.com>
2025-04-23 16:22:23 +0900

    feat : BaseEntity 생성

---
+++ src/main/java/com/outsourcing/outsourcingproject/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.outsourcing.outsourcingproject.common.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+
+
+}
