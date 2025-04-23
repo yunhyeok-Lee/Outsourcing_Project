d4e69df895e1b993387b2d35e0e6180f871a092f
romin014 <sohyun014@naver.com>
2025-04-24 00:11:07 +0900

    docs : Add StoreEntity storeEntity 추가, user 머지 후 연관관계 설정(ManyToOne)

--- src/main/java/com/outsourcing/outsourcingproject/domain/store/entity/StoreEntity.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/store/entity/StoreEntity.java
@@ -1,4 +1,36 @@
 package com.outsourcing.outsourcingproject.domain.store.entity;
 
-public class StoreEntity {
+import java.time.LocalTime;
+
+import com.outsourcing.outsourcingproject.common.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "stores")
+@NoArgsConstructor
+public class StoreEntity extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String status;
+	private LocalTime openTime;
+	private LocalTime closeTime;
+	private int minOrderAnount;
+	private String address;
+	private Boolean isDeleted;
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+
 }
