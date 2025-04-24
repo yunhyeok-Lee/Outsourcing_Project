24bf04d09562b263cc81eed46a346a240f259e51
romin014 <sohyun014@naver.com>
2025-04-24 15:29:58 +0900

    docs: Add Dto responseDto,requestDto 추가

---
+++ src/main/java/com/outsourcing/outsourcingproject/domain/store/dto/StoreResponseDto.java
@@ -0,0 +1,27 @@
+package com.outsourcing.outsourcingproject.domain.store.dto;
+
+import java.time.LocalTime;
+
+import lombok.Getter;
+
+@Getter
+public class StoreResponseDto {
+	private final Long id;
+	private final String status;
+	private final String name;
+	private final LocalTime openTime;
+	private final LocalTime closeTime;
+	private final int minOrderAmount;
+	private final String address;
+
+	public StoreResponseDto(Long id, String status, String name, LocalTime openTime,
+		LocalTime closeTime, int minOrderAmount, String address) {
+		this.id = id;
+		this.status = status;
+		this.name = name;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		this.minOrderAmount = minOrderAmount;
+		this.address = address;
+	}
+}
