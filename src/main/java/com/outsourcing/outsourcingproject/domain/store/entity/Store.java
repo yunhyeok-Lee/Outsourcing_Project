c4567081cb3aa745732095497e4dec991cea44f1
Jess <jcscene@gmail.com>
2025-04-24 11:27:23 +0900

    docs : 엔티티 클래스명 수정

--- src/main/java/com/outsourcing/outsourcingproject/domain/store/entity/Store.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/store/entity/Store.java
@@ -1,9 +1,9 @@
 package com.outsourcing.outsourcingproject.domain.store.entity;
 
-import java.awt.*;
 import java.time.LocalTime;
 
 import com.outsourcing.outsourcingproject.common.entity.BaseEntity;
+import com.outsourcing.outsourcingproject.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,7 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,13 +21,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "stores")
 @NoArgsConstructor
-public class StoreEntity extends BaseEntity {
-	//id identity 설정
+public class Store extends BaseEntity {
+	// id identity 설정
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	//null 값 넣을 수 없도록 제한
+	// null 값 넣을 수 없도록 제한
 	@Column(nullable = false)
 	private String name;
 
@@ -46,19 +45,19 @@ public class StoreEntity extends BaseEntity {
 
 	@Column(nullable = false)
 	private String address;
-	//isDeleted의 디폴트 값 false
+	// isDeleted의 디폴트 값 false
 	@Column(nullable = false)
 	private Boolean isDeleted = false;
 
-	//user 테이블과 다대일 연관관계 설정
-	//LAZY 로딩 사용, 반드시 user_id와 맵핑되어야 함
+	// user 테이블과 다대일 연관관계 설정
+	// LAZY 로딩 사용, 반드시 user_id와 맵핑되어야 함
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
-	//menu 테이블과 일대다 연관관계 설정
-	@OneToMany(mappedBy = "store")
-	private List<Menu> menus;
+	// menu 테이블과 일대다 연관관계 설정
+	// @OneToMany(mappedBy = "store")
+	// private List<Menu> menus;
 	//
 	// @OneToMany(mappedBy = "store")
 	// private List<Order> orders;
