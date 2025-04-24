a9056237424b712ccfdb7fa6c84865a082f77367
romin014 <sohyun014@naver.com>
2025-04-25 00:06:00 +0900

    feat : valid message Notblank valid에 예외 메시지 추가

--- src/main/java/com/outsourcing/outsourcingproject/domain/store/dto/StoreRequestDto.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/store/dto/StoreRequestDto.java
@@ -4,21 +4,23 @@ import java.time.LocalTime;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
 /*
  * 가게 생성 요청 dto
  * */
+@Setter
 @Getter
 public class StoreRequestDto {
-	@NotBlank
+	@NotBlank(message = "가게 이름을 입력해주세요.")
 	private String name;
-	@NotBlank
+	@NotBlank(message = "오픈 시간을 입력해주세요.")
 	private LocalTime openTime;
-	@NotBlank
+	@NotBlank(message = "마감 시간을 입력해주세요.")
 	private LocalTime closeTime;
-	@NotBlank
+	@NotBlank(message = "최소주문금액을 입력해주세요.")
 	private int minOrderAmount;
-	@NotBlank
+	@NotBlank(message = "가게 주소를 입력해주세요.")
 	private String address;
 
 	public StoreRequestDto(String name, LocalTime openTime, LocalTime closeTime, int minOrderAmount, String address) {
