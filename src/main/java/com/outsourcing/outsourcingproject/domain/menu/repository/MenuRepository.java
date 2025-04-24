a277521611c0ebd262283a953a8d4435dd7d3806
yunhyeok_lee <yuy9611@gmail.com>
2025-04-24 15:53:41 +0900

    feat : MenuRepository 생성

--- src/main/java/com/outsourcing/outsourcingproject/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/menu/repository/MenuRepository.java
@@ -1,4 +1,7 @@
 package com.outsourcing.outsourcingproject.domain.menu.repository;
 
-public class MenuRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.outsourcing.outsourcingproject.domain.menu.entity.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
