1a36ed7aac4f80008454113779d59581de0d2a98
yunhyeok_lee <yuy9611@gmail.com>
2025-04-25 14:33:34 +0900

    docs : GlobalExceptionHandler 내용 추가

--- src/main/java/com/outsourcing/outsourcingproject/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/outsourcing/outsourcingproject/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,13 @@
 package com.outsourcing.outsourcingproject.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(UnauthorizedException.class)
+	public ResponseEntity<?> handleUnauthorized(UnauthorizedException ex) {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+	}
 }
