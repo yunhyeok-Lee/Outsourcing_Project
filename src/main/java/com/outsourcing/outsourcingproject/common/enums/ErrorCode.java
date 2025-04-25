6339ff09c787c935d1407c36c30328348068d4ed
yunhyeok_lee <yuy9611@gmail.com>
2025-04-25 14:33:00 +0900

    feat : ErrorCode 추가

--- src/main/java/com/outsourcing/outsourcingproject/common/enums/ErrorCode.java
+++ src/main/java/com/outsourcing/outsourcingproject/common/enums/ErrorCode.java
@@ -6,7 +6,8 @@ public enum ErrorCode implements BaseCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."),
 	ALREADY_DEACTIVATED_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 유저입니다."),
 	CONFLICT_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
-	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호입니다.");
+	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호입니다."),
+	UNAUTHORIZED_MENU_ACCESS(HttpStatus.FORBIDDEN, "사장님만 메뉴를 등록할 수 있습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
