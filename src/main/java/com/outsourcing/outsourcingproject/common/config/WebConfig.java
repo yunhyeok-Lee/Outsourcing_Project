f76893945ca1efa531f4311e72a3e14e9fe72482
yunhyeok_lee <yuy9611@gmail.com>
2025-04-25 20:29:37 +0900

    feat : WebConfig 생성

---
+++ src/main/java/com/outsourcing/outsourcingproject/common/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.outsourcing.outsourcingproject.common.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.outsourcing.outsourcingproject.common.util.JwtAuthenticationFilter;
+import com.outsourcing.outsourcingproject.common.util.JwtUtil;
+import com.outsourcing.outsourcingproject.common.util.RoleCheckFilter;
+
+@Configuration
+public class WebConfig {
+	@Bean
+	public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter(JwtUtil jwtUtil) {
+		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtAuthenticationFilter(jwtUtil));
+		registrationBean.addUrlPatterns("/api/*");
+		registrationBean.setOrder(1); // 인증 먼저
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean<RoleCheckFilter> roleCheckFilter() {
+		FilterRegistrationBean<RoleCheckFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new RoleCheckFilter());
+		registrationBean.addUrlPatterns("/api/*");
+		registrationBean.setOrder(2); // 권한 다음
+		return registrationBean;
+	}
+}
