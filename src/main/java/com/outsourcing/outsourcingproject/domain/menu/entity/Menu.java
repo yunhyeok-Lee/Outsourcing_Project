ca0d402ad1938dfff61791fc4b73836355a0025a
yunhyeok_lee <yuy9611@gmail.com>
2025-04-25 14:33:56 +0900

    docs : Menu 수정

--- src/main/java/com/outsourcing/outsourcingproject/domain/menu/entity/Menu.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/menu/entity/Menu.java
@@ -1,7 +1,5 @@
 package com.outsourcing.outsourcingproject.domain.menu.entity;
 
-import com.outsourcing.outsourcingproject.domain.store.entity.Store;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,14 +9,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import com.outsourcing.outsourcingproject.common.entity.BaseEntity;
+import com.outsourcing.outsourcingproject.domain.store.entity.Store;
+
 @Entity
 @Getter
-@Table(name = "menus")
+@Table(name = "/stores/{storesId}/menu")// 뒤에 s 를 붙이는 경우는 예약어인 경우에만!! 예를들면 users 와같은거만
 @NoArgsConstructor
-public class Menu {
+public class Menu extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +42,18 @@ public class Menu {
 
 	@Column(nullable = false)
 	private String menuType;
+
+	@Column(nullable = false)
+	private Boolean isDeleted = false;
+
+	// id 제외한 생성자
+	@Builder
+	public Menu(Store store, String name, String content, int price, String menuType, Boolean isDeleted) {
+		this.store = store;
+		this.name = name;
+		this.content = content;
+		this.price = price;
+		this.menuType = menuType;
+		this.isDeleted = isDeleted;
+	}
 }
