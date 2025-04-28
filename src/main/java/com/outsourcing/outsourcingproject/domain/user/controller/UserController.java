1b8011add357e6cd7f03f3c781f8e152fcda5e37
kmchaejin <chaejin040@gmail.com>
2025-04-29 03:11:52 +0900

    feat : cookie 설정에 sameSite 설정 추가

--- src/main/java/com/outsourcing/outsourcingproject/domain/user/controller/UserController.java
+++ src/main/java/com/outsourcing/outsourcingproject/domain/user/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.outsourcing.outsourcingproject.domain.user.controller;
 
+import java.time.Duration;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +26,6 @@ import com.outsourcing.outsourcingproject.domain.user.dto.UserRequestDto;
 import com.outsourcing.outsourcingproject.domain.user.dto.UserResponseDto;
 import com.outsourcing.outsourcingproject.domain.user.service.UserService;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -76,14 +79,15 @@ public class UserController {
 	}
 
 	private void setToken(HttpServletResponse response, LoginResponseDto dto) {
-		Cookie cookie = new Cookie("refreshToken", dto.getRefreshToken());
-
-		cookie.setHttpOnly(true);
-		cookie.setSecure(false);
-		cookie.setPath("/");
-		cookie.setMaxAge(7 * 24 * 60 * 60);
+		ResponseCookie cookie = ResponseCookie.from("refreshToken", dto.getRefreshToken())
+			.httpOnly(true) // JS 접근 방지
+			.secure(true)   // HTTPS 환경에서만 전송
+			.sameSite("Strict")  // 다른 사이트 요청에서는 이 쿠키를 전송하지 않음
+			.path("/")
+			.maxAge(Duration.ofMinutes(60 * 24 * 7))
+			.build();
 
 		response.setHeader("Authorization", dto.getAccessToken());
-		response.addCookie(cookie);
+		response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 	}
 }
