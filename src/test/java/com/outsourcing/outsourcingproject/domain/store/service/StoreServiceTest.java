b8467be0fb56bd7b8fc01c9d6644dae48d0cfc88
romin014 <sohyun014@naver.com>
2025-04-29 12:03:49 +0900

    test : Creat 테스트 코드 추가

--- src/test/java/com/outsourcing/outsourcingproject/domain/store/service/StoreServiceTest.java
+++ src/test/java/com/outsourcing/outsourcingproject/domain/store/service/StoreServiceTest.java
@@ -3,6 +3,9 @@ package com.outsourcing.outsourcingproject.domain.store.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalTime;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,8 +15,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.outsourcing.outsourcingproject.common.enums.ErrorCode;
 import com.outsourcing.outsourcingproject.common.exception.CustomException;
 import com.outsourcing.outsourcingproject.common.util.EntityFetcher;
-import com.outsourcing.outsourcingproject.common.util.JwtUtil;
-import com.outsourcing.outsourcingproject.domain.menu.repository.MenuRepository;
 import com.outsourcing.outsourcingproject.domain.store.dto.StoreRequestDto;
 import com.outsourcing.outsourcingproject.domain.store.entity.Store;
 import com.outsourcing.outsourcingproject.domain.store.repository.StoreRepository;
@@ -25,16 +26,59 @@ class StoreServiceTest {
 	private StoreRepository storeRepository;
 
 	@Mock
-	private MenuRepository menuRepository;
-
-	@Mock
 	private EntityFetcher entityFetcher;
-
-	@Mock
-	private JwtUtil jwtUtil;
 
 	@InjectMocks
 	private StoreService storeService;
+
+	@Test
+	void 가게_생성_성공() {
+		// given
+		Long userId = 1L;
+		StoreRequestDto request = new StoreRequestDto("가게", "09:00", "22:00", 10000, "서울");
+
+		User user = new User();
+		Store store = Store.builder()
+			.name("가게")
+			.openTime(LocalTime.of(9, 0))
+			.closeTime(LocalTime.of(22, 0))
+			.minOrderAmount(10000)
+			.address("서울")
+			.reviewCounts(0)
+			.build();
+
+		when(entityFetcher.getUserOrThrow(userId)).thenReturn(user);
+		when(storeRepository.countByUser_IdAndIsDeletedFalse(userId)).thenReturn(0);
+		when(storeRepository.save(any(Store.class))).thenReturn(store);
+
+		// when
+		var result = storeService.createStore(userId, request);
+
+		// then
+		assertThat(result.getName()).isEqualTo("가게");
+		assertThat(result.getMinOrderAmount()).isEqualTo(10000);
+	}
+
+	@Test
+	void 가게_이름으로_조회_성공() {
+		// given
+		String name = "가게";
+		Store store = Store.builder()
+			.name(name)
+			.openTime(LocalTime.of(9, 0))
+			.closeTime(LocalTime.of(22, 0))
+			.reviewCounts(5)
+			.build();
+
+		when(storeRepository.findByNameAndIsDeletedFalse(name)).thenReturn(List.of(store));
+
+		// when
+		var result = storeService.findByName(name);
+
+		// then
+		assertThat(result.getStore()).hasSize(1);
+		assertThat(result.getStore().get(0).getName()).isEqualTo(name);
+	}
 
 	@Test
 	void 가게_최대개수초과이면_CustomException() {
@@ -83,3 +127,4 @@ class StoreServiceTest {
 			.hasMessageContaining(ErrorCode.STORE_ALREADY_DELETED.getMessage());
 	}
 }
+
